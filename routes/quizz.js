c239b0651432e9f80830fd091bcfabf75bdca90c
Nicolas Poppe <NicolasPoppe123@gmail.com>
2021-12-14 15:28:49 +0100

    ajout des authorize

--- routes/quizz.js
+++ routes/quizz.js
@@ -2,71 +2,72 @@ var express = require('express');
 var router = express.Router();
 const { Quizz } = require("../model/quizz");
 const db = require('../db')
+const { authorize } = require('../utils/authorize');
 
 const quizzModel = new Quizz();
 
-router.get('/isLiked/', async function(req, res, next) {
+router.get('/isLiked/', authorize, async function(req, res, next) {
     const result = await quizzModel.isLike(req.query.id_quizz,req.query.id_user);
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
 // TODO : a tester quand la db sera peuplée
-router.get('/mostLiked', async function(req, res, next) {
+router.get('/mostLiked', authorize, async function(req, res, next) {
     const result = await quizzModel.get6MoreLikedQuizz();
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
-router.get('/recherche/:critere', async function(req, res, next) {
+router.get('/recherche/:critere', authorize, async function(req, res, next) {
     const result = await quizzModel.getQuizzByCritere(req.params.critere);
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
-router.get('/explorer', async function(req, res, next) {
+router.get('/explorer', authorize, async function(req, res, next) {
     const result = await quizzModel.getQuizzExplorer();
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
-router.get('/byEmail/:email', async function(req, res, next) {
+router.get('/byEmail/:email', authorize, async function(req, res, next) {
     const result = await quizzModel.getQuizzByEmail(req.params.email);
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
-router.get('/abonnements/:id', async function(req, res, next) {
+router.get('/abonnements/:id', authorize, async function(req, res, next) {
     const result = await quizzModel.getQuizzAbonnements(req.params.id);
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
-router.get('/likes/:id', async function(req, res, next) {
+router.get('/likes/:id', authorize, async function(req, res, next) {
     const result = await quizzModel.getNbLikes(req.params.id);
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
-router.get('/forUser/:id', async function(req, res, next) {
+router.get('/forUser/:id', authorize, async function(req, res, next) {
     const result = await quizzModel.getQuizzByUser(req.params.id);
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
-router.get('/:id', async function(req, res, next) {
+router.get('/:id', authorize, async function(req, res, next) {
    const result = await quizzModel.getQuizzById(req.params.id);
    if(!result) res.sendStatus(404).end();
    res.send(result);
 });
 
-router.get('/', async function(req, res, next) {
+router.get('/', authorize, async function(req, res, next) {
     const result = await quizzModel.getAllQuizz();
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
-router.post('/', async function(req, res, next) {
+router.post('/', authorize, async function(req, res, next) {
     if (!req.body ||
         (req.body.hasOwnProperty("name") && req.body.name.length === 0) ||
         (req.body.hasOwnProperty("id_creator") && req.body.id_creator.length === 0)
@@ -78,7 +79,7 @@ router.post('/', async function(req, res, next) {
 
 
 
-router.delete("/unlike/", async function (req, res) {
+router.delete("/unlike/", authorize, async function (req, res) {
     // Send an error code '400 Bad request' if the body parameters are not valid
     if (
         !req.body ||
@@ -93,13 +94,13 @@ router.delete("/unlike/", async function (req, res) {
     res.send(like);
 });
 
-router.delete('/:id', async function(req, res, next) {
+router.delete('/:id', authorize, async function(req, res, next) {
       const result = await quizzModel.deleteQuizz(req.params.id);
       if(!result) res.sendStatus(404).end();
       res.send(result);
 })
 
-router.post("/likes/", async function (req, res) {
+router.post("/likes/", authorize, async function (req, res) {
     // Send an error code '400 Bad request' if the body parameters are not valid
     if (
         !req.body ||
