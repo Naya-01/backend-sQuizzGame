193262197a69946404d619b6c8fa5444c2142d58
Nicolas Poppe <NicolasPoppe123@gmail.com>
2021-12-19 14:13:08 +0100

    ajout des commentaires

--- routes/quizz.js
+++ routes/quizz.js
@@ -6,62 +6,70 @@ const { authorize } = require('../utils/authorize');
 
 const quizzModel = new Quizz();
 
+// GET /quizz/isLiked?id : test si un quizz est liké
 router.get('/isLiked/', authorize, async function(req, res, next) {
     const result = await quizzModel.isLike(req.query.id_quizz,req.query.id_user);
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
-// TODO : a tester quand la db sera peuplée
+// GET /quizz/mostLiked : pour avoir les quizz les plus likés (tendances)
 router.get('/mostLiked', authorize, async function(req, res, next) {
     const result = await quizzModel.get6MoreLikedQuizz();
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
+// GET /quizz/recherche/{critere} : pour avoir les quizz qui ont pour critere critere
 router.get('/recherche/:critere', authorize, async function(req, res, next) {
     const result = await quizzModel.getQuizzByCritere(req.params.critere);
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
+// GET /quizz/explorer : pour avoir 9 quizz aléatoires dans la db (explorer)
 router.get('/explorer', authorize, async function(req, res, next) {
     const result = await quizzModel.getQuizzExplorer();
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
+// GET /quizz/byEmail/{email} : pour avoir les quizz d'un utilisateur via son email
 router.get('/byEmail/:email', authorize, async function(req, res, next) {
     const result = await quizzModel.getQuizzByEmail(req.params.email);
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
+// GET /quizz/abonnements/{id} : pour avoir les quizz des utilisateurs qui sont dans ses abonnements
 router.get('/abonnements/:id', authorize, async function(req, res, next) {
     const result = await quizzModel.getQuizzAbonnements(req.params.id);
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
+// GET /likes/{id} : pour avoir les likes d'un quizz via son id
 router.get('/likes/:id', authorize, async function(req, res, next) {
     const result = await quizzModel.getNbLikes(req.params.id);
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
+// GET /quizz/forUser/{id} : pour avoir les quizz d'un utilisateur via son id
 router.get('/forUser/:id', authorize, async function(req, res, next) {
     const result = await quizzModel.getQuizzByUser(req.params.id);
     if(!result) res.sendStatus(404).end();
     res.send(result);
 })
 
+// GET /quizz/{id} : pour avoir un quizz via son id
 router.get('/:id', authorize, async function(req, res, next) {
    const result = await quizzModel.getQuizzById(req.params.id);
    if(!result) res.sendStatus(404).end();
    res.send(result);
 });
 
-
+// POST /quizz/ : pour ajouter un quizz à la db
 router.post('/', authorize, async function(req, res, next) {
     if (!req.body ||
         (req.body.hasOwnProperty("name") && req.body.name.length === 0) ||
@@ -73,7 +81,7 @@ router.post('/', authorize, async function(req, res, next) {
 })
 
 
-
+// DELETE /quizz/unlike : pour supprimer un like
 router.delete("/unlike/", authorize, async function (req, res) {
     // Send an error code '400 Bad request' if the body parameters are not valid
     if (
@@ -89,12 +97,14 @@ router.delete("/unlike/", authorize, async function (req, res) {
     res.send(like);
 });
 
+// DELETE /quizz/{id} : supprimer un quizz
 router.delete('/:id', authorize, async function(req, res, next) {
       const result = await quizzModel.deleteQuizz(req.params.id);
       if(!result) res.sendStatus(404).end();
       res.send(result);
 })
 
+// POST /quizz/likes/ : pour liker un quizz
 router.post("/likes/", authorize, async function (req, res) {
     // Send an error code '400 Bad request' if the body parameters are not valid
     if (
